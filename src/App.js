1a45262e29513ad2739ea40fc38bf9de88da9f53
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2021-11-14 17:40:06 +0530

    Clean starting files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+{
+  /* <Router>
+      <NavBar />
+      <Switch>
+        <UserContext.Provider value={userProviderValue}>
+          <Route exact path="/" component={LandingPage}></Route>
+          <Route exact path="/about" component={AboutUsPage}></Route>
+          <Route exact path="/FAQ" component={FAQPage}></Route>
+          <Route exact path="/signup" component={SignUpPage}></Route>
+          <ProtectedRoute
+            exact
+            path="/products"
+            component={ProductsPage}
+          ></ProtectedRoute>
+          <Route exact path="/plans" component={PlansPage}></Route>
+          <Route
+            exact
+            path="/productInfo/:productId"
+            component={ProductInfoPage}
+          ></Route>
+        </UserContext.Provider>
+      </Switch>
+      <Footer />
+    </Router> */
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
